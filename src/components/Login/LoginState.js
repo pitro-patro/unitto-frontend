624e4828265b4e737cbc00cc452ff5eb12312c9c
정수범 <teller75@naver.com>
2022-03-30 17:09:07 +0900

    loginstate style

--- src/components/Login/LoginState.js
+++ src/components/Login/LoginState.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { KAKAO_AUTH_URL } from "../../../src/secretKey";
 import kakaoLoginImage from '../../img/kakao_login_medium_narrow.png'
 import request from "../../request";
+import "../../styles/LoginState.css"
 
 const LoginState = () => {
     const [jwtToken, setJwtToken] = useState(
@@ -31,7 +32,7 @@ const LoginState = () => {
 
     if(!jwtToken){
         return(
-            <div>
+            <div className="loginStateContainer">
                 <a href={KAKAO_AUTH_URL}>
                     <img alt='kakao_login_button' src = {kakaoLoginImage}/>
                 </a>
@@ -40,21 +41,18 @@ const LoginState = () => {
     }
 
     return(
-        <div>
-            <div>
-                <h3>
-                    사용자 정보
-                </h3>
-                <ul>
-                    {
-                        Object.keys(userData).map((key, index)=>
-                            <li key={index}>{key} : {userData[key]}</li>)
-                    }
-                </ul>
-                
-            </div>
+        <div className="loginStateContainer">
+            <ul className="userdata_ul">
+                <li className="userdata_li">{userData.name}님</li>
+                <li className="userdata_li"> / </li>
+                <li className="userdata_li">{userData.email}</li>
+                {/* {
+                    Object.keys(userData).map((key, index)=>
+                        <li key={index}>{key} : {userData[key]}</li>)
+                } */}
+            </ul>
 
-            <button onClick={logoutHandler}>
+            <button className="buttonContainer" onClick={logoutHandler}>
                 로그아웃
             </button>
         </div>
