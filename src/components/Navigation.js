d2daedb0923cbd2ef2e2132c53b3c91dbd29c33b
정수범 <teller75@naver.com>
2022-04-10 16:55:00 +0900

    Navigation styling

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -7,9 +7,9 @@ const Navigation = () =>{
     return(
         <div className="navigationContainer">
             <ul className="navigation_ul">
-                <li className="navigation_li"><Link to={"/"}>홈</Link></li>
-                <li className="navigation_li"><Link to={"/my/number"}>내 번호</Link></li>
-                <li className="navigation_li"><Link to={"/number"}>번호생성</Link></li>
+                <li className="navigation_li"><Link className="link_text" style={{ textDecoration: 'none' }} to={"/"}>홈</Link></li>
+                <li className="navigation_li"><Link className="link_text hover1" style={{ textDecoration: 'none' }} to={"/my/number"}>내 번호</Link></li>
+                <li className="navigation_li"><Link className="link_text hover2" style={{ textDecoration: 'none' }} to={"/number"}>번호생성</Link></li>
             </ul>
         </div>
     )
